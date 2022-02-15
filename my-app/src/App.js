3dd372838f5321a0cbc3347b24825002b4366d67
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2022-02-15 16:36:31 -0600

    props working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Product from "./Product";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello worled</h1>
+      <Product name = 'Amazon Echo' description = 'your AI Assistant' price = {59.99}/>
     </div>
   );
 }
